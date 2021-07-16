f8713da7f442e3a982de1af0d3e255d16703380e
shiory602 <suzushioufo@gmail.com>
2021-07-16 15:38:58 -0700

    React.Fragment package.json

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,11 +4,6 @@ import 'react-dates/initialize';
 import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
-// import "react-date-range/dist/styles.css";
-// import "react-date-range/dist/theme/default.css";
-// import { DateRangePicker } from "react-date-range";
-import { Button } from "@material-ui/core";
-import PeopleIcon from "@material-ui/icons/People"
 import { useHistory } from "react-router-dom";
 import moment from "moment";
 
@@ -41,9 +36,6 @@ export default function Search() {
                 focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
                 onFocusChange={focusedInput => setFocusedInput(focusedInput)} // PropTypes.func.isRequired,
             />
-            {/* <H2>Number of guests <PeopleIcon/></H2>
-            <Input min={0} defaultValue={2} type="number" />
-            <Submit><Button onClick={() => history.push('./search')}>Search Airbnb</Button></Submit> */}
         </Body>
     );
 }
@@ -51,41 +43,4 @@ export default function Search() {
 const Body = styled.div`
     position: absolute;
     top: 120px;
-    /* left: 10px; */
-    /* width: 300px; */
-    /* z-index: 999; */
 `
-
-// const H2 = styled.h2`
-//     display: flex;
-//     align-items: center;
-//     justify-content: space-between;
-//     width: 558px;
-//     padding: 10px;
-//     background-color: white;
-//     position: absolute;
-//     left: 0;
-//     top: 335px;
-// `
-
-// const Input = styled.input`
-//     width: 558px;
-//     padding: 20px;
-//     position: absolute;
-//     left: 0;
-//     height: 30px;
-//     top: 390px;
-//     border: none;
-//         &:focus {
-//             outline-width: 0;
-//         }
-// `
-// const Submit = styled.button`
-//         position: absolute;
-//         left: 0;
-//         top: 430px;
-//         text-transform: inherit;
-//         background-color: #ff7779;
-//         color: white;
-//         width: 560px;
-// `
